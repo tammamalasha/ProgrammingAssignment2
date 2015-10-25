bfa41eb89732957c8ea47567a8d34a6ac9dcc299
tammamalasha <tammamalasha@gmail.com>
2015-10-25 22:54:01 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,6 +7,7 @@
 # 2. get the value of the matrix
 # 3. set the value of inverse of the matrix
 # 4. get the value of inverse of the matrix
+
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
     set <- function(y) {
@@ -26,6 +27,7 @@ makeCacheMatrix <- function(x = matrix()) {
 # setinverse function.
 
 # This function assumes that the matrix is always invertible.
+
 cacheSolve <- function(x, ...) {
     inv <- x$getinverse()
     if(!is.null(inv)) {
